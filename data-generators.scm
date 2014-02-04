5973dd3927a3dadf979e991b577fe033d221f7d1
David Krentzlin <github@lisp-unleashed.de>
2014-02-04 12:29:42 +0100

    added gen-sample-of

--- data-generators.scm
+++ data-generators.scm
@@ -1,4 +1,15 @@
 (module data-generators
-  ()
+  (gen-current-fixnum-min
+   gen-current-fixnum-max
+   gen-current-default-size
+   gen-int8 gen-uint8 gen-int16 gen-uint16 gen-int32 gen-uint32 gen-int64 gen-uint64
+   gen-bool gen-char gen-pair-of gen-list-of gen-tuple-of gen-vector-of gen-string-of
+   gen-fixnum gen-hash-table-of gen-sample-of
+   between at-most at-least exactly)
   (import chicken scheme)
+  (use (prefix random-bsd bsd:)
+       (only ports with-output-to-string)
+       (only srfi-1 iota list-tabulate list-ref)
+       srfi-14 srfi-69)
+
   (include "data-generators-impl.scm"))
