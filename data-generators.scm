952afc7eb5117c84b640a91f6df9e004984aee60
certainty <certainty@localhost>
2014-02-04 17:38:29 +0100

    more tests

--- data-generators.scm
+++ data-generators.scm
@@ -1,13 +1,12 @@
 (module data-generators
-
   (%random-fixnum
    gen-current-fixnum-min
    gen-current-fixnum-max
    gen-current-default-size
    gen-int8 gen-uint8 gen-int16 gen-uint16 gen-int32 gen-uint32 gen-int64 gen-uint64
-   gen-bool gen-char gen-real gen-pair-of gen-list-of gen-tuple-of gen-vector-of gen-string-of
+   gen-bool gen-char gen-real gen-pair-of gen-list-of gen-alist-of gen-tuple-of gen-vector-of gen-string-of
    gen-fixnum gen-hash-table-of gen-sample-of
-   between at-most at-least)
+   between at-most at-least with-size)
   (import chicken scheme)
   (use (prefix random-bsd bsd:)
        (only ports with-output-to-string)
--- tests/run.scm
+++ tests/run.scm
@@ -6,12 +6,12 @@
 (define (between? a x y) (and (>= a x) (<= a y)))
 
 (test-group "gen-bool"
-    (test-assert
-     (in? (gen-bool) (list #t #f))))
+  (test-assert
+    (in? (gen-bool) (list #t #f))))
 
 (test-group "gen-fixnum"
     (test-assert
-      (between? (gen-fixnum) (gen-current-fixnum-min) (gen-current-fixnum-max)))
+     (between? (gen-fixnum) (gen-current-fixnum-min) (gen-current-fixnum-max)))
     (test-assert
      (between? (between gen-fixnum 0 4) 0 4))
     (test-assert
@@ -28,3 +28,60 @@
      (>= (at-least gen-real 1.0) 1.0))
     (test-assert
      (<= (at-most gen-real 2.0) 2.0)))
+
+(test-group "gen-pair-of"
+    (test-assert "produces a pair"
+      (pair? (gen-pair-of gen-fixnum gen-fixnum)))
+    (test-assert "car is element of expected set"
+      (fixnum? (car (gen-pair-of gen-fixnum gen-fixnum))))
+    (test-assert "cdr is element of expected set"
+      (fixnum? (cdr (gen-pair-of gen-fixnum gen-fixnum)))))
+
+(test-group "gen-tuple-of"
+    (test "produces tuple of given size"
+          3 (length (gen-tuple-of gen-fixnum gen-fixnum gen-fixnum)))
+    (test "each element is element of expected set"
+          #t
+          (every fixnum? (gen-tuple-of gen-fixnum gen-fixnum))))
+
+(test-group "gen-list-of"
+    (test-assert "produces a list"
+       (list? (gen-list-of gen-fixnum)))
+    (test-assert "each element is part of expected set"
+       (every fixnum? (gen-list-of gen-fixnum)))
+    (test "it generates list of given length"
+      3
+      (with-size 3
+        (length (gen-list-of gen-fixnum)))))
+
+(test-group "gen-alist-of"
+   (test-assert "produces a list"
+      (list? (gen-alist-of gen-char gen-fixnum)))
+   (test-assert "every key is of expected set"
+      (every (compose char? car) (gen-alist-of gen-char gen-fixnum)))
+   (test-assert "every value is of expected set"
+      (every (compose fixnum? cdr) (gen-alist-of gen-char gen-fixnum)))
+   (test "it generates alist of given length"
+       4
+       (with-size 4 (length (gen-alist-of gen-char gen-fixnum)))))
+
+(test-group "gen-vector-of"
+   (test-assert "produces a vector"
+      (vector? (gen-vector-of gen-fixnum)))
+   (test-assert "every element is element of expected set"
+      (every fixnum? (vector->list (gen-vector-of gen-fixnum))))
+   (test "it generates vector of given length"
+      4
+      (with-size 4
+        (vector-length (gen-vector-of gen-fixnum)))))
+
+(test-group "gen-string-of"
+    (test-assert "produces a string"
+      (string? (gen-string-of)))
+    (test-assert "every element is within the given char-set"
+                 (every (cut char-set-contains? char-set:graphic <>)
+                        (string->list
+                         (gen-string-of char-set:graphic))))
+    (test "it generates a string of the given length"
+       10
+       (with-size 10 (string-length (gen-string-of)))))
