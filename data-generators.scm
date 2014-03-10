7a6f49a814c199168a148b0b541b85ac563601e5
certainty <certainty@localhost>
2014-03-10 16:06:07 +0100

    replaced primitive generators with new generator abstraction. Added parameter boundary checks outside the generation phase

--- data-generators.scm
+++ data-generators.scm
@@ -15,12 +15,15 @@
   (%random-fixnum
    gen-current-fixnum-min
    gen-current-fixnum-max
-   gen-current-default-size
+   <-
+   ;gen-current-default-size
    gen-int8 gen-uint8 gen-int16 gen-uint16 gen-int32 gen-uint32 gen-int64 gen-uint64
-   gen-bool gen-char gen-real gen-pair-of gen-list-of gen-alist-of gen-tuple-of gen-vector-of gen-string-of
-   gen-fixnum gen-hash-table-of gen-sample-of
-   make-sizer sizer/lb sizer/ub
-   between at-most at-least with-size gen->sequence run-sequence)
+   gen-bool gen-char gen-fixnum gen-real
+   ;gen-real gen-pair-of gen-list-of gen-alist-of gen-tuple-of gen-vector-of gen-string-of
+   ;gen-fixnum gen-hash-table-of gen-sample-of
+   ;make-sizer sizer/lb sizer/ub
+   ;between at-most at-least with-size gen->sequence run-sequence
+   )
   (import chicken scheme)
   (use (prefix random-bsd bsd:)
        (only ports with-output-to-string)
