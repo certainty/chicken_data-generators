d5280c3bf6fcd46c29089e0280c2ce44c6c7ecf8
David Krentzlin <github@lisp-unleashed.de>
2014-03-10 16:06:07 +0100

    replaced primitive generators with new generator abstraction. Added parameter boundary checks outside the generation phase

--- data-generators-impl.scm
+++ data-generators-impl.scm
@@ -11,13 +11,22 @@
 ;; A full copy of the GPL license can be found at
 ;; <http://www.gnu.org/licenses/>.
 
+(define-syntax generator
+  (syntax-rules ()
+    ((_ ?body ...)
+     (lambda () ?body ...))))
+
+;; take amount elements from gen and return it in a list
+(define (<-* amount gen)
+  (map (lambda _ (gen)) (iota amount)))
+
+(define (<- gen)
+  (gen))
 
 (define gen-current-fixnum-min (make-parameter -65536))
 (define gen-current-fixnum-max (make-parameter 65536))
 
 (define (%random-fixnum lo hi)
-  (unless (<= lo hi)
-    (error '%random-fixnum "upper bound must be <= lower bound" lo hi))
   (let ((range (- hi lo -1)))
     (inexact->exact (+ (bsd:random-integer range) lo))))
 
@@ -25,27 +34,28 @@
 ;; will most likely be used to feed some foreign code
 ;; the ranges have been selected to conform to those present on most platforms
 
-(define gen-int8   (cut %random-fixnum -127 127))
-(define gen-uint8  (cut %random-fixnum 0 255))
-(define gen-int16  (cut %random-fixnum -32767 32767))
-(define gen-uint16 (cut %random-fixnum 0 65535))
-(define gen-int32  (cut %random-fixnum -2147483647 2147483647))
-(define gen-uint32 (cut %random-fixnum 0 4294967295))
-(define gen-int64  (cut %random-fixnum -9223372036854775807 9223372036854775807))
-(define gen-uint64 (cut %random-fixnum 0 18446744073709551615))
+(define gen-fixnum
+  (case-lambda
+    (()      (gen-fixnum (gen-current-fixnum-min) (gen-current-fixnum-max)))
+    ((upper) (gen-fixnum (gen-current-fixnum-min) upper))
+    ((lower upper)
+     (unless (<= lower upper)
+       (error "upper bound must be <= lower bound"))
+     (generator (%random-fixnum lower upper)))))
 
+(define-syntax define-fixed-range-generator
+  (syntax-rules ()
+    ((_ ?name ?lower ?upper)
+     (define (?name) (generator (%random-fixnum ?lower ?upper))))))
 
-;; generic generator for fixnum allows to size the resulting number
-(define make-sizer cons)
-(define sizer/lb car)
-(define sizer/ub cdr)
-
-(define (between  gen lb ub) (gen (make-sizer lb ub)))
-(define (at-most  gen ub)    (gen (make-sizer (gen-current-fixnum-min) ub)))
-(define (at-least gen lb)    (gen (make-sizer lb (gen-current-fixnum-max))))
-
-(define (gen-fixnum #!optional (sizer (make-sizer (gen-current-fixnum-min) (gen-current-fixnum-max))))
-  (%random-fixnum (sizer/lb sizer) (sizer/ub sizer)))
+(define-fixed-range-generator gen-int8 -127 127)
+(define-fixed-range-generator gen-uint8 0 255)
+(define-fixed-range-generator gen-int16 -32767 32767)
+(define-fixed-range-generator gen-uint16 0 65535)
+(define-fixed-range-generator gen-int32 -2147483647 2147483647)
+(define-fixed-range-generator gen-uint32 0 4294967295)
+(define-fixed-range-generator gen-int64 -9223372036854775807 9223372036854775807)
+(define-fixed-range-generator gen-uint64 0 18446744073709551615)
 
 (define (%clamp val lower upper)
   (cond
@@ -57,12 +67,15 @@
   (let ((ub (+ size start)))
     (%clamp (+ start (* size (bsd:random-real))) start ub)))
 
-(define (gen-real #!optional (sizer (make-sizer 0.0 1.0)))
-  (let* ((start (sizer/lb sizer))
-         (size  (- (sizer/ub sizer) start)))
-    (%random-real size start)))
+(define gen-real
+  (case-lambda
+    (()      (gen-real 0.0 1.0))
+    ((upper) (gen-real 0.0 upper))
+    ((lower upper)
+     (generator (%random-real (- upper lower) lower)))))
 
-(define (gen-bool) (zero? (bsd:random-fixnum 2)))
+(define (gen-bool)
+  (generator (zero? (bsd:random-fixnum 2))))
 
 (define (%random-char charset)
   (let loop ((cursor (char-set-cursor charset)) (i (bsd:random-fixnum (sub1 (char-set-size charset)))))
@@ -70,95 +83,99 @@
      ((zero? i) (char-set-ref charset cursor))
      (else (loop (char-set-cursor-next charset cursor) (sub1 i))))))
 
-
-
-(define (sizer->charset sizer)
+(define (boundaries->charset lower upper)
   (define (maybe-apply v p t) (if (p v) (t v) v))
 
-  (let ((lo (max 0 (maybe-apply (sizer/lb sizer) char? char->integer)))
-        (hi (max 0 (min 255 (maybe-apply (sizer/ub sizer) char? char->integer)))))
+  (let ((lo (max 0 (maybe-apply lower char? char->integer)))
+        (hi (max 0 (min 255 (maybe-apply upper char? char->integer)))))
     (unless (<= lo hi)
       (error 'sizer->charset "lower bound must be <= upper bound"))
     (list->char-set (map integer->char (iota (add1 (- hi lo)) lo)))))
 
-(define (gen-char #!optional (sizer-or-charset char-set:graphic))
-  (if (char-set? sizer-or-charset)
-      (%random-char sizer-or-charset)
-      (%random-char (sizer->charset sizer-or-charset))))
-
-;; combinators
-(define gen-current-default-size (make-parameter (cut gen-uint8)))
-
-(define-syntax with-size
-  (syntax-rules ()
-    ((_ (lb . ub) body0 ...)
-     (parameterize ((gen-current-default-size (cut between gen-fixnum lb ub)))
-       body0 ...))
-    ((_ size body0 ...)
-     (parameterize ((gen-current-default-size (constantly size)))
-       body0 ...))))
-
-(define (gen-sample-of list-of-gen)
-  (let* ((l   (length list-of-gen))
-         (gen (list-ref list-of-gen  (between gen-fixnum 0 (sub1 l)))))
-    (gen)))
-
-(define (gen-pair-of gen1 gen2)
-  (cons (gen1) (gen2)))
-
-(define (gen-tuple-of . gens)
-  (map (lambda (g) (g)) gens))
-
-(define gen-list-of
+(define gen-char
   (case-lambda
-    ((gen) (gen-list-of gen (gen-current-default-size)))
-    ((gen size)
-     (list-tabulate (size) (lambda _ (gen))))))
+    (() (gen-char char-set:graphic))
+    ((charset)
+     (unless (char-set? charset)
+       (error "You need to supply a char-set"))
+     (generator (%random-char charset)))
+    ((lower upper)
+     (let ((charset (boundaries->charset lower upper)))
+       (generator (%random-char charset))))))
 
-(define  gen-alist-of
-  (case-lambda
-    ((key-gen value-gen) (gen-list-of (lambda () (gen-pair-of key-gen value-gen)) (gen-current-default-size)))
-    ((key-gen value-gen size)
-     (gen-list-of (lambda () (gen-pair-of key-gen value-gen)) size))))
+;; ;; combinators
+;; (define gen-current-default-size (make-parameter (cut gen-uint8)))
 
-(define gen-vector-of
-  (case-lambda
-    ((gen) (gen-vector-of gen (gen-current-default-size)))
-    ((gen size)
-     (let ((size (size)))
-       (do ((i 0 (add1 i))
-            (vec (make-vector size)))
-           ((>= i size) vec)
-         (vector-set! vec i (gen)))))))
+;; (define-syntax with-size
+;;   (syntax-rules ()
+;;     ((_ (lb . ub) body0 ...)
+;;      (parameterize ((gen-current-default-size (cut between gen-fixnum lb ub)))
+;;        body0 ...))
+;;     ((_ size body0 ...)
+;;      (parameterize ((gen-current-default-size (constantly size)))
+;;        body0 ...))))
 
-(define gen-string-of
-  (case-lambda
-    (()    (gen-string-of char-set:graphic (gen-current-default-size)))
-    ((cs) (gen-string-of cs (gen-current-default-size)))
-    ((cs size)
-     (let ((size (size)))
-       (with-output-to-string
-         (lambda ()
-           (do ((i 0 (add1 i)))
-               ((>= i size))
-             (display (gen-char cs)))))))))
+;; (define (gen-sample-of list-of-gen)
+;;   (let* ((l   (length list-of-gen))
+;;          (gen (list-ref list-of-gen  (between gen-fixnum 0 (sub1 l)))))
+;;     (gen)))
 
-(define gen-hash-table-of
-  (case-lambda
-    ((key-gen value-gen) (gen-hash-table-of key-gen value-gen (gen-current-default-size)))
-    ((key-gen value-gen size)
-     (let ((size (size)))
-       (do ((i 0 (add1 i))
-            (ht (make-hash-table)))
-           ((>= i size) ht)
-         (hash-table-set! ht (key-gen) (value-gen)))))))
+;; (define (gen-pair-of gen1 gen2)
+;;   (cons (gen1) (gen2)))
 
-(define-syntax gen->sequence
-  (syntax-rules ()
-    ((_ amount body0 ...)
-     (lambda (proc)
-       (do ((i 1 (add1 i)))
-           ((>= i amount))
-         (proc (begin body0 ...)))))))
+;; (define (gen-tuple-of . gens)
+;;   (map (lambda (g) (g)) gens))
 
-(define (run-sequence seq proc) (seq proc))
+;; (define gen-list-of
+;;   (case-lambda
+;;     ((gen) (gen-list-of gen (gen-current-default-size)))
+;;     ((gen size)
+;;      (list-tabulate (size) (lambda _ (gen))))))
+
+;; (define  gen-alist-of
+;;   (case-lambda
+;;     ((key-gen value-gen) (gen-list-of (lambda () (gen-pair-of key-gen value-gen)) (gen-current-default-size)))
+;;     ((key-gen value-gen size)
+;;      (gen-list-of (lambda () (gen-pair-of key-gen value-gen)) size))))
+
+;; (define gen-vector-of
+;;   (case-lambda
+;;     ((gen) (gen-vector-of gen (gen-current-default-size)))
+;;     ((gen size)
+;;      (let ((size (size)))
+;;        (do ((i 0 (add1 i))
+;;             (vec (make-vector size)))
+;;            ((>= i size) vec)
+;;          (vector-set! vec i (gen)))))))
+
+;; (define gen-string-of
+;;   (case-lambda
+;;     (()    (gen-string-of char-set:graphic (gen-current-default-size)))
+;;     ((cs) (gen-string-of cs (gen-current-default-size)))
+;;     ((cs size)
+;;      (let ((size (size)))
+;;        (with-output-to-string
+;;          (lambda ()
+;;            (do ((i 0 (add1 i)))
+;;                ((>= i size))
+;;              (display (gen-char cs)))))))))
+
+;; (define gen-hash-table-of
+;;   (case-lambda
+;;     ((key-gen value-gen) (gen-hash-table-of key-gen value-gen (gen-current-default-size)))
+;;     ((key-gen value-gen size)
+;;      (let ((size (size)))
+;;        (do ((i 0 (add1 i))
+;;             (ht (make-hash-table)))
+;;            ((>= i size) ht)
+;;          (hash-table-set! ht (key-gen) (value-gen)))))))
+
+;; (define-syntax gen->sequence
+;;   (syntax-rules ()
+;;     ((_ amount body0 ...)
+;;      (lambda (proc)
+;;        (do ((i 1 (add1 i)))
+;;            ((>= i amount))
+;;          (proc (begin body0 ...)))))))
+
+;; (define (run-sequence seq proc) (seq proc))
--- tests/run.scm
+++ tests/run.scm
@@ -10,21 +10,19 @@
     ((_  gen lower upper)
      (test-group (symbol->string (quote gen))
        (test-assert
-        (between? (gen) lower upper))))))
+        (between? (<- (gen)) lower upper))))))
 
 (test-group "gen-bool"
   (test-assert
-    (in? (gen-bool) (list #t #f))))
+   (in? (<- (gen-bool)) (list #t #f))))
 
 (test-group "gen-fixnum"
     (test-assert
-     (between? (gen-fixnum) (gen-current-fixnum-min) (gen-current-fixnum-max)))
+     (between? (<- (gen-fixnum)) (gen-current-fixnum-min) (gen-current-fixnum-max)))
     (test-assert
-     (between? (between gen-fixnum 0 4) 0 4))
+     (between? (<- (gen-fixnum 4)) (gen-current-fixnum-min) 4))
     (test-assert
-     (>=  (at-least gen-fixnum 2) 2))
-    (test-assert
-     (<=  (at-most gen-fixnum 2) 2)))
+     (between? (<- (gen-fixnum 2 4)) 2 4)))
 
 (test-fixed-range gen-int8 -127 127)
 (test-fixed-range gen-uint8 0 255)
@@ -37,92 +35,90 @@
 
 (test-group "gen-real"
     (test-assert
-     (between? (gen-real) 0.0 1.0))
+     (between? (<- (gen-real)) 0.0 1.0))
     (test-assert
-     (between? (between gen-real 1.0 2.0) 1.0 2.0))
+     (between? (<- (gen-real 1.0)) 0.0 1.0))
     (test-assert
-     (>= (at-least gen-real 1.0) 1.0))
-    (test-assert
-     (<= (at-most gen-real 2.0) 2.0)))
+     (between? (<- (gen-real 1.0 2.0)) 1.0 2.0)))
 
-(test-group "gen-pair-of"
-    (test-assert "produces a pair"
-      (pair? (gen-pair-of gen-fixnum gen-fixnum)))
-    (test-assert "car is element of expected set"
-      (fixnum? (car (gen-pair-of gen-fixnum gen-fixnum))))
-    (test-assert "cdr is element of expected set"
-      (fixnum? (cdr (gen-pair-of gen-fixnum gen-fixnum)))))
+;; (test-group "gen-pair-of"
+;;     (test-assert "produces a pair"
+;;       (pair? (gen-pair-of gen-fixnum gen-fixnum)))
+;;     (test-assert "car is element of expected set"
+;;       (fixnum? (car (gen-pair-of gen-fixnum gen-fixnum))))
+;;     (test-assert "cdr is element of expected set"
+;;       (fixnum? (cdr (gen-pair-of gen-fixnum gen-fixnum)))))
 
-(test-group "gen-tuple-of"
-    (test "produces tuple of given size"
-          3 (length (gen-tuple-of gen-fixnum gen-fixnum gen-fixnum)))
-    (test "each element is element of expected set"
-          #t
-          (every fixnum? (gen-tuple-of gen-fixnum gen-fixnum))))
+;; (test-group "gen-tuple-of"
+;;     (test "produces tuple of given size"
+;;           3 (length (gen-tuple-of gen-fixnum gen-fixnum gen-fixnum)))
+;;     (test "each element is element of expected set"
+;;           #t
+;;           (every fixnum? (gen-tuple-of gen-fixnum gen-fixnum))))
 
-(test-group "gen-list-of"
-    (test-assert "produces a list"
-       (list? (gen-list-of gen-fixnum)))
-    (test-assert "each element is part of expected set"
-       (every fixnum? (gen-list-of gen-fixnum)))
-    (test "it generates list of given length"
-      3
-      (with-size 3
-        (length (gen-list-of gen-fixnum)))))
+;; (test-group "gen-list-of"
+;;     (test-assert "produces a list"
+;;        (list? (gen-list-of gen-fixnum)))
+;;     (test-assert "each element is part of expected set"
+;;        (every fixnum? (gen-list-of gen-fixnum)))
+;;     (test "it generates list of given length"
+;;       3
+;;       (with-size 3
+;;         (length (gen-list-of gen-fixnum)))))
 
-(test-group "gen-alist-of"
-   (test-assert "produces a list"
-      (list? (gen-alist-of gen-char gen-fixnum)))
-   (test-assert "every key is of expected set"
-      (every (compose char? car) (gen-alist-of gen-char gen-fixnum)))
-   (test-assert "every value is of expected set"
-      (every (compose fixnum? cdr) (gen-alist-of gen-char gen-fixnum)))
-   (test "it generates alist of given length"
-       4
-       (with-size 4 (length (gen-alist-of gen-char gen-fixnum)))))
+;; (test-group "gen-alist-of"
+;;    (test-assert "produces a list"
+;;       (list? (gen-alist-of gen-char gen-fixnum)))
+;;    (test-assert "every key is of expected set"
+;;       (every (compose char? car) (gen-alist-of gen-char gen-fixnum)))
+;;    (test-assert "every value is of expected set"
+;;       (every (compose fixnum? cdr) (gen-alist-of gen-char gen-fixnum)))
+;;    (test "it generates alist of given length"
+;;        4
+;;        (with-size 4 (length (gen-alist-of gen-char gen-fixnum)))))
 
-(test-group "gen-vector-of"
-   (test-assert "produces a vector"
-      (vector? (gen-vector-of gen-fixnum)))
-   (test-assert "every element is element of expected set"
-      (every fixnum? (vector->list (gen-vector-of gen-fixnum))))
-   (test "it generates vector of given length"
-      4
-      (with-size 4
-        (vector-length (gen-vector-of gen-fixnum)))))
+;; (test-group "gen-vector-of"
+;;    (test-assert "produces a vector"
+;;       (vector? (gen-vector-of gen-fixnum)))
+;;    (test-assert "every element is element of expected set"
+;;       (every fixnum? (vector->list (gen-vector-of gen-fixnum))))
+;;    (test "it generates vector of given length"
+;;       4
+;;       (with-size 4
+;;         (vector-length (gen-vector-of gen-fixnum)))))
 
-(test-group "gen-string-of"
-    (test-assert "produces a string"
-      (string? (gen-string-of)))
-    (test-assert "every element is within the given char-set"
-                 (every (cut char-set-contains? char-set:graphic <>)
-                        (string->list
-                         (gen-string-of char-set:graphic))))
-    (test "it generates a string of the given length"
-       10
-       (with-size 10 (string-length (gen-string-of)))))
+;; (test-group "gen-string-of"
+;;     (test-assert "produces a string"
+;;       (string? (gen-string-of)))
+;;     (test-assert "every element is within the given char-set"
+;;                  (every (cut char-set-contains? char-set:graphic <>)
+;;                         (string->list
+;;                          (gen-string-of char-set:graphic))))
+;;     (test "it generates a string of the given length"
+;;        10
+;;        (with-size 10 (string-length (gen-string-of)))))
 
-(test-group "gen-hash-table-of"
-    (test-assert "produces a hash-table"
-       (hash-table? (gen-hash-table-of gen-fixnum gen-fixnum)))
-    (test-assert "every key is element of expected set"
-       (every fixnum? (hash-table-keys (gen-hash-table-of gen-fixnum gen-fixnum))))
-    (test-assert "every value is element of expected set"
-       (every fixnum? (hash-table-values (gen-hash-table-of gen-fixnum gen-fixnum))))
-    (test "it generates a hash-table of given size"
-      10
-      (with-size 10 (hash-table-size (gen-hash-table-of gen-fixnum gen-fixnum)))))
+;; (test-group "gen-hash-table-of"
+;;     (test-assert "produces a hash-table"
+;;        (hash-table? (gen-hash-table-of gen-fixnum gen-fixnum)))
+;;     (test-assert "every key is element of expected set"
+;;        (every fixnum? (hash-table-keys (gen-hash-table-of gen-fixnum gen-fixnum))))
+;;     (test-assert "every value is element of expected set"
+;;        (every fixnum? (hash-table-values (gen-hash-table-of gen-fixnum gen-fixnum))))
+;;     (test "it generates a hash-table of given size"
+;;       10
+;;       (with-size 10 (hash-table-size (gen-hash-table-of gen-fixnum gen-fixnum)))))
 
-(test-group "with-size"
-    (test-assert "fixed size" (= 2 (length  (with-size 2 (gen-list-of gen-fixnum)))))
-    (test-assert "range" (between? (length  (with-size (2 . 4) (gen-list-of gen-fixnum))) 2 4)))
+;; (test-group "with-size"
+;;     (test-assert "fixed size" (= 2 (length  (with-size 2 (gen-list-of gen-fixnum)))))
+;;     (test-assert "range" (between? (length  (with-size (2 . 4) (gen-list-of gen-fixnum))) 2 4)))
 
-(test-group "gen->sequence"
-    (test-assert "generates a procedure" (procedure? (gen->sequence 2 (gen-list-of gen-fixnum))))
-    (let ((runs 1)
-          (seq (gen->sequence 3 (gen-list-of gen-fixnum))))
-      (test "applies proc amount times"
-            3
-            (begin
-              (seq (lambda (value) (set! runs (add1 runs))))
-              runs))))
+;; (test-group "gen->sequence"
+;;     (test-assert "generates a procedure" (procedure? (gen->sequence 2 (gen-list-of gen-fixnum))))
+;;     (let ((runs 1)
+;;           (seq (gen->sequence 3 (gen-list-of gen-fixnum))))
+;;       (test "applies proc amount times"
+;;             3
+;;             (begin
+;;               (seq (lambda (value) (set! runs (add1 runs))))
+;;               runs))))
