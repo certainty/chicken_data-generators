1e8ed71b89727f39ab00dad7e44c8f6f7d6bc615
David Krentzlin <github@lisp-unleashed.de>
2014-03-10 16:56:38 +0100

    updated tests and implementet gen-for-each as well as gen-constant

--- data-generators.scm
+++ data-generators.scm
@@ -12,18 +12,15 @@
 ;; <http://www.gnu.org/licenses/>.
 
 (module data-generators
-  (%random-fixnum
-   gen-current-fixnum-min
+  (gen-current-fixnum-min
    gen-current-fixnum-max
-   <-
-   ;gen-current-default-size
+   gen-current-default-size
+   generator <- <-* gen-for-each
+   gen-constant
    gen-int8 gen-uint8 gen-int16 gen-uint16 gen-int32 gen-uint32 gen-int64 gen-uint64
-   gen-bool gen-char gen-fixnum gen-real
-   ;gen-real gen-pair-of gen-list-of gen-alist-of gen-tuple-of gen-vector-of gen-string-of
-   ;gen-fixnum gen-hash-table-of gen-sample-of
-   ;make-sizer sizer/lb sizer/ub
-   ;between at-most at-least with-size gen->sequence run-sequence
-   )
+   gen-bool gen-char gen-fixnum gen-real gen-sample-of gen-pair-of gen-tuple-of
+   gen-list-of gen-alist-of gen-vector-of gen-string gen-hash-table-of
+   with-size)
   (import chicken scheme)
   (use (prefix random-bsd bsd:)
        (only ports with-output-to-string)
--- tests/run.scm
+++ tests/run.scm
@@ -5,13 +5,35 @@
 (define (in? x ls) (not (null? (member x ls))))
 (define (between? a x y) (and (>= a x) (<= a y)))
 
-
 (define-syntax test-fixed-range
   (syntax-rules ()
     ((_  gen lower upper)
      (test-group (symbol->string (quote gen))
        (test-assert
         (between? (<- (gen)) lower upper))))))
+
+(test-group "gen-for-each"
+    (let ((runs 1))
+      (test "applies proc amount times"
+            3
+            (begin
+              (gen-for-each 3 (lambda (value) (set! runs (add1 runs))) (gen-list-of (gen-fixnum)))
+              runs))))
+
+(test-group "<-"
+            (test "it takes one element from gen"
+                  3
+                  (<- (gen-sample-of (list 3)))))
+
+(test-group "<-*"
+            (test "it takes n elements form gen"
+                  (list 3 3 3)
+                  (<-* 3 (gen-sample-of (list 3)))))
+
+(test-group "gen-constant"
+            (test "it constantly returns the given value"
+                  42
+                  (<- (gen-constant 42))))
 
 (test-group "gen-bool"
   (test-assert
@@ -24,7 +46,8 @@
      (between? (<- (gen-fixnum 4)) (gen-current-fixnum-min) 4))
     (test-assert
      (between? (<- (gen-fixnum 2 4)) 2 4))
-    (test-error (gen-fixnum 4 2)))
+    (test-error "lower bound <= upper bound"
+     (gen-fixnum 4 2)))
 
 (test-fixed-range gen-int8 -127 127)
 (test-fixed-range gen-uint8 0 255)
@@ -42,7 +65,7 @@
      (between? (<- (gen-real 1.0)) 0.0 1.0))
     (test-assert
      (between? (<- (gen-real 1.0 2.0)) 1.0 2.0))
-    (test-error
+    (test-error "lower bound <= upper bound"
      (gen-real 2.0 1.0)))
 
 (test-group "gen-char"
@@ -51,86 +74,82 @@
     (test-assert
      (char-set-contains? char-set:digit (<- (gen-char char-set:digit))))
     (test-assert
-     (char-set-contains? (char-set #\a #\b #\c) (<- (gen-char #\a #\c)))))
+     (char-set-contains? (char-set #\a #\b #\c) (<- (gen-char #\a #\c))))
+    (test-error "lower bound <= upper bound"
+     (gen-char #\z #\a)))
 
-;; (test-group "gen-pair-of"
-;;     (test-assert "produces a pair"
-;;       (pair? (gen-pair-of gen-fixnum gen-fixnum)))
-;;     (test-assert "car is element of expected set"
-;;       (fixnum? (car (gen-pair-of gen-fixnum gen-fixnum))))
-;;     (test-assert "cdr is element of expected set"
-;;       (fixnum? (cdr (gen-pair-of gen-fixnum gen-fixnum)))))
+(test-group "gen-sample-of"
+    (test-assert (between? (<- (gen-sample-of (iota 10))) 0 9))
+    (test-assert (in? (<- (gen-sample-of (list #\a #\b #\c))) (list #\a #\b #\c))))
 
-;; (test-group "gen-tuple-of"
-;;     (test "produces tuple of given size"
-;;           3 (length (gen-tuple-of gen-fixnum gen-fixnum gen-fixnum)))
-;;     (test "each element is element of expected set"
-;;           #t
-;;           (every fixnum? (gen-tuple-of gen-fixnum gen-fixnum))))
+(test-group "gen-pair-of"
+    (test-assert "produces a pair"
+                 (pair? (<- (gen-pair-of (gen-fixnum) (gen-fixnum)))))
+    (test-assert "car is element of expected set"
+                 (fixnum? (car (<- (gen-pair-of (gen-fixnum) (gen-fixnum))))))
+    (test-assert "cdr is element of expected set"
+                 (fixnum? (cdr (<- (gen-pair-of (gen-fixnum) (gen-fixnum)))))))
 
-;; (test-group "gen-list-of"
-;;     (test-assert "produces a list"
-;;        (list? (gen-list-of gen-fixnum)))
-;;     (test-assert "each element is part of expected set"
-;;        (every fixnum? (gen-list-of gen-fixnum)))
-;;     (test "it generates list of given length"
-;;       3
-;;       (with-size 3
-;;         (length (gen-list-of gen-fixnum)))))
+(test-group "gen-tuple-of"
+    (test "produces tuple of given size"
+          3 (length (<- (gen-tuple-of (gen-fixnum) (gen-fixnum) (gen-fixnum)))))
+    (test "each element is element of expected set"
+          #t
+          (every fixnum? (<- (gen-tuple-of (gen-fixnum) (gen-fixnum))))))
 
-;; (test-group "gen-alist-of"
-;;    (test-assert "produces a list"
-;;       (list? (gen-alist-of gen-char gen-fixnum)))
-;;    (test-assert "every key is of expected set"
-;;       (every (compose char? car) (gen-alist-of gen-char gen-fixnum)))
-;;    (test-assert "every value is of expected set"
-;;       (every (compose fixnum? cdr) (gen-alist-of gen-char gen-fixnum)))
-;;    (test "it generates alist of given length"
-;;        4
-;;        (with-size 4 (length (gen-alist-of gen-char gen-fixnum)))))
+(test-group "gen-list-of"
+    (test-assert "produces a list"
+                 (list? (<- (gen-list-of (gen-fixnum)))))
+    (test-assert "each element is part of expected set"
+                 (every fixnum? (<- (gen-list-of (gen-fixnum)))))
+    (test "it generates list of given length"
+      3
+      (with-size 3
+                 (length (<- (gen-list-of (gen-fixnum)))))))
 
-;; (test-group "gen-vector-of"
-;;    (test-assert "produces a vector"
-;;       (vector? (gen-vector-of gen-fixnum)))
-;;    (test-assert "every element is element of expected set"
-;;       (every fixnum? (vector->list (gen-vector-of gen-fixnum))))
-;;    (test "it generates vector of given length"
-;;       4
-;;       (with-size 4
-;;         (vector-length (gen-vector-of gen-fixnum)))))
+(test-group "gen-alist-of"
+   (test-assert "produces a list"
+                (list? (<- (gen-alist-of (gen-char) (gen-fixnum)))))
+   (test-assert "every key is of expected set"
+                (every (compose char? car) (<- (gen-alist-of (gen-char) (gen-fixnum)))))
+   (test-assert "every value is of expected set"
+                (every (compose fixnum? cdr) (<- (gen-alist-of (gen-char) (gen-fixnum)))))
+   (test "it generates alist of given length"
+       4
+       (with-size 4 (length (<- (gen-alist-of (gen-char) (gen-fixnum)))))))
 
-;; (test-group "gen-string-of"
-;;     (test-assert "produces a string"
-;;       (string? (gen-string-of)))
-;;     (test-assert "every element is within the given char-set"
-;;                  (every (cut char-set-contains? char-set:graphic <>)
-;;                         (string->list
-;;                          (gen-string-of char-set:graphic))))
-;;     (test "it generates a string of the given length"
-;;        10
-;;        (with-size 10 (string-length (gen-string-of)))))
+(test-group "gen-vector-of"
+   (test-assert "produces a vector"
+                (vector? (<- (gen-vector-of (gen-fixnum)))))
+   (test-assert "every element is element of expected set"
+                (every fixnum?  (vector->list (<- (gen-vector-of (gen-fixnum))))))
+   (test "it generates vector of given length"
+      4
+      (with-size 4
+                 (vector-length (<- (gen-vector-of (gen-fixnum)))))))
 
-;; (test-group "gen-hash-table-of"
-;;     (test-assert "produces a hash-table"
-;;        (hash-table? (gen-hash-table-of gen-fixnum gen-fixnum)))
-;;     (test-assert "every key is element of expected set"
-;;        (every fixnum? (hash-table-keys (gen-hash-table-of gen-fixnum gen-fixnum))))
-;;     (test-assert "every value is element of expected set"
-;;        (every fixnum? (hash-table-values (gen-hash-table-of gen-fixnum gen-fixnum))))
-;;     (test "it generates a hash-table of given size"
-;;       10
-;;       (with-size 10 (hash-table-size (gen-hash-table-of gen-fixnum gen-fixnum)))))
+(test-group "gen-string"
+    (test-assert "produces a string"
+                 (string? (<- (gen-string))))
+    (test-assert "every element is within the given char-set"
+                 (every (cut char-set-contains? char-set:graphic <>)
+                        (string->list
+                         (<- (gen-string char-set:graphic)))))
+    (test "it generates a string of the given length"
+       10
+       (with-size 10 (string-length (<- (gen-string))))))
 
-;; (test-group "with-size"
-;;     (test-assert "fixed size" (= 2 (length  (with-size 2 (gen-list-of gen-fixnum)))))
-;;     (test-assert "range" (between? (length  (with-size (2 . 4) (gen-list-of gen-fixnum))) 2 4)))
+(test-group "gen-hash-table-of"
+    (test-assert "produces a hash-table"
+                 (hash-table? (<- (gen-hash-table-of (gen-fixnum) (gen-fixnum)))))
+    (test-assert "every key is element of expected set"
+                 (every fixnum? (hash-table-keys (<- (gen-hash-table-of (gen-fixnum) (gen-fixnum))))))
+    (test-assert "every value is element of expected set"
+                 (every fixnum? (hash-table-values (<- (gen-hash-table-of (gen-fixnum) (gen-fixnum))))))
+    (test "it generates a hash-table of given size"
+      10
+      (with-size 10 (hash-table-size (<- (gen-hash-table-of (gen-fixnum) (gen-fixnum)))))))
 
-;; (test-group "gen->sequence"
-;;     (test-assert "generates a procedure" (procedure? (gen->sequence 2 (gen-list-of gen-fixnum))))
-;;     (let ((runs 1)
-;;           (seq (gen->sequence 3 (gen-list-of gen-fixnum))))
-;;       (test "applies proc amount times"
-;;             3
-;;             (begin
-;;               (seq (lambda (value) (set! runs (add1 runs))))
-;;               runs))))
+(test-group "with-size"
+            (test-assert "fixed size" (= 2 (length  (with-size 2 (<- (gen-list-of (gen-fixnum)))))))
+            (test-assert "range" (between? (length  (with-size (2 . 4) (<- (gen-list-of (gen-fixnum))))) 2 4)))
